e35e1a348aad2504f078ef883f8ef0993e1c5c64
Yim Samaune <samaune@gmail.com>
2025-11-08 16:38:59 +0700

    Add PowerShell script to calculate directory sizes
    This script calculates the size of files and directories in a specified path, displaying results in gigabytes.

---
+++ dirsize.ps1
@@ -0,0 +1,18 @@
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$Path="C:\",
+)
+
+Get-ChildItem $Path -Force |
+ForEach-Object {
+    if ($_.PSIsContainer) {
+        $size = (Get-ChildItem $_.FullName -Recurse -Force -ErrorAction SilentlyContinue |
+                 Measure-Object -Property Length -Sum).Sum
+    } else {
+        $size = $_.Length
+    }
+    [PSCustomObject]@{
+        Name = $_.FullName
+        SizeGB = [math]::Round($size / 1GB, 2)
+    }
+} | Sort-Object SizeGB -Descending | Format-Table -AutoSize
