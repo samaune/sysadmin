934afe9d6b7927a91be70bff28d832338c6d250e
samaune <samaune@gmail.com>
2025-11-16 19:07:02 +0700

    enhance automate code

--- scripts/pfx.ps1
+++ scripts/pfx.ps1
@@ -1,80 +1,144 @@
-<#
-.SYNOPSIS
-    Automation script SSL renewal with arguments and -DomainName -Force switch.
-
-.DESCRIPTION
-    Import a PFX certificate, checks if it exists in LocalMachine\My, and does not expired today.
-#>
-
-param(
-  [Parameter(Mandatory=$true, Position=0)]
-  [string]$DomainName,
-  
+param( 
   # [Parameter(Mandatory=$false)]
-  # [string]$Password = "",
+  # [string]$PfxPwd = "",
 
   [Parameter(Mandatory=$false, HelpMessage="Force import even if certificate exists or not expired")]
   [switch]$Force
 )
-
-if(!$DomainName) {
-  Write-Host "Automation script SSL renewal with arguments and -DomainName -Force switch"
-  exit 0
+$env = (Get-Content ".env" | Where-Object { $_ -match '=' }) -replace '"', '' -join "`n" | ConvertFrom-StringData
+function Get-DayExpiredHttp($domain){
+    $tcp = New-Object System.Net.Sockets.TcpClient; 
+    $tcp.Connect($domain, 443);
+    $ssl = New-Object System.Net.Security.SslStream($tcp.GetStream(), $false, ({$true}));
+    $ssl.AuthenticateAsClient($domain);
+    $resCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $ssl.RemoteCertificate;
+    $tcp.Close(); $ssl.Close();
+    $remainingDay = ([math]::Ceiling(($resCert.NotAfter - (Get-Date)).TotalDays));
+    Write-Host "[$domain] has expired in $remainingDay day(s)"
+    return $remainingDay
 }
 
-$port=443;
-$pfxPath = "uploads\$DomainName.pfx"
 
-# $tcp=New-Object System.Net.Sockets.TcpClient; 
-# $tcp.Connect($DomainName, $port);
-# $ssl=New-Object System.Net.Security.SslStream($tcp.GetStream(), $false, ({$true}));
-# $ssl.AuthenticateAsClient($DomainName);
-# $resCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $ssl.RemoteCertificate;
-# $tcp.Close(); $ssl.Close();
-# $remainingDay = ([math]::Ceiling(($resCert.NotAfter - (Get-Date)).TotalDays));
-$remainingDay = 1
-
-if (($remainingDay -eq 0) -or ($Force)) {
-  # Download new PFX
+function Get-DownloadPfx($pfxUrl, $pfxPath, $pfxPwd){
   try {
+    $dir = Split-Path $pfxPath -Parent
+    # Create directory if not exists
+    if (-not (Test-Path $dir)) {
+      New-Item -ItemType Directory -Path $dir | Out-Null
+    }
     if (Test-Path $pfxPath) {
       $pfxNewPath = $pfxPath -replace "\.pfx$", "_$(Get-Date -Format "yyyyMMdd").pfx";
       Move-Item -Path $pfxPath -Destination  $pfxNewPath -Force
-      Write-Host "Moved $pfxPath"
+      Write-Host "Moved backup $pfxPath"
     }
-    $PfxUrl= $Env:PFX_URL + "/$domainName.pfx"; 
-    Invoke-WebRequest -Uri $PfxUrl -OutFile $pfxPath -UseBasicParsing -SkipCertificateCheck
-    Write-Host "Downloaded PFX to $pfxPath"    
-  } catch {
-    Write-Error "Failed to download PFX: $_"
-    exit 1
-  }
-  
-  $pfxPwd = $Env:PFX_PWD | ConvertTo-SecureString 
-  $cert = Get-PfxCertificate -FilePath $pfxPath -Password $pfxPwd
-  $existingCert = Get-ChildItem Cert:\LocalMachine\My ` | Where-Object { $_.Thumbprint -eq ($cert.Thumbprint) } `
-  
-  if (!$existingCert) {
-    $pfx= Import-PfxCertificate -FilePath $pfxPath -Password $pfxPwd -CertStoreLocation Cert:\LocalMachine\My
-    Write-Host "Certificate imported with thumbprint:" $pfx.Thumbprint
-    $thumbprint = $pfx.Thumbprint
-  }else{
-    Write-Host "Certificate existing with thumbprint:" $existingCert.Thumbprint
-    $thumbprint = $existingCert.Thumbprint
-  }
-  
-  # Binding - IIS Binding update cert to IIS (Requied enabled DNI)
-  $binding = "${DomainName}:${port}"
-  Write-Host "Updating binding $binding..."
-  # netsh http delete sslcert hostnameport=$binding
-  netsh http update sslcert hostnameport=$binding certhash=$thumbprint certstorename=My appid='{98525227-7F70-4B89-908D-BE5F94026C65}'
-  netsh http add sslcert hostnameport=$binding certhash=$thumbprint certstorename=My appid='{98525227-7F70-4B89-908D-BE5F94026C65}'
 
-  # Cleanup - Remove expired certificate
-  Get-ChildItem Cert:\LocalMachine\My `
-  | Where-Object { ($_.NotAfter -lt (Get-Date)) } `
-  | Remove-Item
-} else {
-  Write-Host "Certificate does not expire today. Use -Force to re-import."
-  exit 0
+    $encoded = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($env.PFX_BASIC_AUTH))
+    Invoke-WebRequest -Uri $pfxUrl `
+    -Headers @{ Authorization = "Basic $encoded" } `
+    -OutFile $pfxPath -UseBasicParsing -SkipCertificateCheck
+    return Get-PfxCertificate -FilePath $pfxPath -Password (ConvertTo-SecureString $pfxPwd -AsPlainText -Force)
+  } catch {
+    Write-Host "Failed to download pfx: $_"
+    return $null
+    # exit 1
+  }
 }
+# Return $False nothin
+function Import-PfxCert($domain, $pfxPath, $pfxPwd) {
+  try {
+    $pfxUrl= $env.PFX_URL + "/$domain.pfx"; 
+    $pfx = Get-DownloadPfx $pfxUrl $pfxPath $pfxPwd
+    if(!$pfx){
+      return $null
+    }
+    $cert = Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Thumbprint -eq ($pfx.Thumbprint) } 
+    if (!$cert) {
+      $cert = Import-PfxCertificate -FilePath $pfxPath `
+      -Password (ConvertTo-SecureString $pfxPwd -AsPlainText -Force) `
+      -CertStoreLocation Cert:\LocalMachine\My
+
+      Write-Host "Certificate imported with thumbprint:" $newCert.Thumbprint
+    }else{
+      Write-Host "Certificate existing with thumbprint:" $cert.Thumbprint
+    }
+
+    return $cert.Thumbprint
+  } catch {
+    return $null
+  }
+}
+
+
+function Get-AppIdByBinding($binding) {
+    $items = netsh http show sslcert
+    $lines = $items -split "`n"
+    for ($i = 0; $i -lt $lines.Length; $i++) {
+      if ($lines[$i] -match "Hostname:port\s*:\s*$binding") {
+        $webBinding = [PSCustomObject]@{
+          AppId = ""
+          CertHash = ""
+        }
+        for ($j = $i; $j -lt $i+10; $j++) {
+          # Capture Cert Hash
+          if ($lines[$j] -match "Certificate Hash\s*:\s*(.+)") {
+              $webBinding.CertHash = $matches[1].Trim()
+          }
+
+          # Capture AppId (always in {GUID} format)
+          if ($lines[$j] -match "Application ID\s*:\s*(\{[0-9a-fA-F\-]+\})") {
+              $webBinding.AppId = $matches[1].Trim()
+              return $webBinding
+          }
+        }
+      }
+    }
+    return $null
+}
+
+function Add-NewWebBinding ([string]$site, [string]$domain, [Int32]$port) {
+  # Import-Module WebAdministration
+  New-WebBinding -Name $site -Protocol "https" -Port $port -HostHeader $domain
+  Set-WebBinding -Name $site -BindingInformation "*:${port}:${domain}" -PropertyName "sslFlags" -Value 1
+}
+
+function Initialize {
+  $port=443;
+  
+  $domain = $env.DOMAIN
+  $site = $env.IIS_SITE
+  $pfxPwd = $env.PFX_PWD
+  $pfxPath = $env.PFX_TMP + "\$domain.pfx"
+  $binding = "${domain}:${port}"
+
+  if (((Get-DayExpiredHttp $domain) -le 0) -or ($Force)) {
+    $thumbprint = Import-PfxCert $domain $pfxPath $pfxPwd
+
+    if ($thumbprint) {
+      $webBinding = Get-AppIdByBinding $binding
+      if($webBinding){
+        $appId = $webBinding.AppId
+        if($webBinding.CertHash -ne $thumbprint){
+          netsh http update sslcert hostnameport=$binding certhash=$thumbprint appid=$appId certstorename=My
+          Write-Host "Updated binding [$binding], $thumbprint, $appId"
+        }
+        Write-Host "No update found"
+      }else{
+        $appId = "{$([guid]::NewGuid())}"
+        Add-NewWebBinding $site $domain $port
+        Write-Host "Add IIS web binding  [$binding], $thumbprint, $appId"
+        # netsh http delete sslcert hostnameport=$binding
+        netsh http add sslcert hostnameport=$binding certhash=$thumbprint appid=$appId certstorename=My
+        Write-Host "Add certificate and assign {$appId} to [$binding], certhash:$thumbprint"
+      }
+    }else{
+      Write-Host "No new pfx certificate imported and updated"
+    }
+
+    # Cleanup - Remove expired certificate
+    Get-ChildItem Cert:\LocalMachine\My | Where-Object { ($_.NotAfter -lt (Get-Date)) } | Remove-Item
+  } else {
+    Write-Host "Certificate does not expire today. Use -Force to re-import."
+  }
+}
+
+Initialize
--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,23 +1,36 @@
 
-$port=443;
-$DomainName = "forms..com"
-$binding = "${DomainName}:${port}"
-$pfxPath = "uploads\$DomainName.pfx"
-
-function Get-AppIdByBinding($binding) {
-    $items = netsh http show sslcert
-    $lines = $items -split "`n"
-    for ($i = 0; $i -lt $lines.Length; $i++) {
-        if ($lines[$i] -match "Hostname:port\s*:\s*$binding") {
-            for ($j = $i; $j -lt $i+10; $j++) {
-                if ($lines[$j] -match "Application ID\s*:\s*(.+)") {
-                    return $matches[1]
-                }
-            }
-        }
-    }
+Import-Module WebAdministration
+function Get-DotEnv() {
+    $envFile = Get-Content ".env" | Where-Object { $_ -match '=' }
+    $envVars = $envFile -replace '"', '' -join "`n" | ConvertFrom-StringData
+    return $envVars
 }
 
-$appId = Get-AppIdByBinding $binding
-$thumbprint = ""
-netsh http update sslcert hostnameport=$binding certhash=$thumbprint certstorename=My appid=$appId
+$env = Get-DotEnv
+$port=443;
+$dnsName = $env.DNS_NAME
+$pfxPwd = $env.PFX_PWD
+$pfxPath = $env.PFX_TMP + "\$dnsName.pfx"
+$binding = "${dnsName}:${port}"
+
+function Add-NewIISBinding(){
+  $site = "forms"
+  $domain = "forms.example.com"
+  $port = 443
+
+  # $cert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Subject -like "*$domain*" }
+  # $thumb = $cert.Thumbprint
+
+  # Create binding
+  New-WebBinding -Name "forms" -Protocol "https" -Port 443 -HostHeader "forms.example.com"
+  Set-WebBinding -Name "forms" -BindingInformation "*:443:forms.example.com" -PropertyName "sslFlags" -Value 1
+
+  # Push-Location IIS:\SslBindings
+  # Get-Item "0.0.0.0!$port!$domain" | Remove-Item -ErrorAction SilentlyContinue
+  # New-Item "0.0.0.0!$port!$domain" -Thumbprint $thumb -SSLFlags 1
+  # Pop-Location
+}
+
+Add-NewIISBinding
+
+Get-ChildItem IIS:\Sites | Where-Object { $_.Bindings.Collection | Where-Object { $_.HostHeader -eq "forms.example.com"} }
